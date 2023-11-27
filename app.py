27f41e0b9276e54abcdb92853a3e35b70e1a1aee
Renan1404 <82480715+Renan1404@users.noreply.github.com>
2023-11-27 01:41:41 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
- 
-app = Flask(__name__) 
- 
-@app.route('/') 
-def home(): 
-   return render_template('index.html') 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/mitologia')
 def mythology():
@@ -16,14 +16,18 @@ def quizzes():
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
-    if request.method == 'POST':     
+    if request.method == 'POST':
         nome = request.form['nome']
         email = request.form['email']
         senha = request.form['senha']
-        
 
         return redirect(url_for('sucesso'))
 
     return render_template('cadastro.html')
-if __name__ == '__main__': 
- app.run(debug=True) 
+
+@app.route('/sucesso')
+def sucesso():
+    return render_template('sucesso.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
